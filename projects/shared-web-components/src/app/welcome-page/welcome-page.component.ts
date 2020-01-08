0203557750d34f762443b8c406edd3ec8ad50c95
Junxi <junxi@cxisoftware.com.au>
2020-01-08 15:24:32 +1100

    add demo html, output web component result

--- projects/shared-web-components/src/app/welcome-page/welcome-page.component.ts
+++ projects/shared-web-components/src/app/welcome-page/welcome-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output ,EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styles:['.component-container{display:flex;flex-direction:column}.component-action{padding:12px;}.component-action input{padding:12px;margin-right:20px;}.component-action input:focus{outline:none;}.component-action button{padding:12px;border:none;outline:none;text-transform:uppercase;color:#fff;background:rgb(20, 128, 218);cursor:pointer;transition:500ms all ease;}.component-action button:hover{background:rgb(16, 100, 168);}.component-h5{padding:12px;background-color:brown;color:#fff}.component-p{padding:12px;background-color:rgb(194, 67, 67);color:#fff}']
 })
 export class WelcomePageComponent implements OnInit {
+  @Output() validate = new EventEmitter<any>();
 
   submitted:boolean =false;
   name:string = '';
@@ -14,4 +16,18 @@ export class WelcomePageComponent implements OnInit {
   ngOnInit() {
   }
 
+  validateFn(){
+    if(this.name){
+      const validateObj = {
+        name:this.name.toLowerCase(),
+        date:new Date(),
+        status:'Success'
+      }
+      console.log('got name',validateObj)
+      this.validate.emit(validateObj);
+    }else{
+      alert('please write your name again');
+      this.submitted = false;
+    }
+  }
 }
