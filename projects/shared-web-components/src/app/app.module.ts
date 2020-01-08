4e4ff33c0ca900b41d4b41f37522f4c6a90af613
Junxi <junxi@cxisoftware.com.au>
2020-01-08 13:21:31 +1100

    remove unnessary files

--- projects/shared-web-components/src/app/app.module.ts
+++ projects/shared-web-components/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 import {createCustomElement} from '@angular/elements';
@@ -9,7 +8,6 @@ import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
     WelcomePageComponent
   ],
   imports: [
@@ -17,11 +15,10 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { 
   constructor(injector:Injector){
     const welcomePageWebComponent = createCustomElement(WelcomePageComponent,{injector:injector})
   }
-
+  ngDoBootstrap() {}
 }
